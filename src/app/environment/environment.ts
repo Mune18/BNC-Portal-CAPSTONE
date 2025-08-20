42522692cfa8ef95c56e9731d26e35200b1857c4
Mune18 <151175266+Mune18@users.noreply.github.com>
2025-08-21 01:17:13 +0800

    Update environment.ts

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,12 +1,14 @@
 export const environment = {
-  production: false,
+  production: true,
   appwriteUrl: 'https://cloud.appwrite.io/v1',
   appwriteProjectId: '67dd969c00272bf0ce80',
   appwriteDatabaseId: '67dd9c370012f6d6fdc0',
+  SecretApiKey: 'standard_1d112a218ba0e4e724f8141e9b1bd36a3976885bf70895f5bfd81ccb50304d5d3facfa479cbffac66478a5d70ceedafc34de54a01db7156d864d497cf2bd71b4987e0e8d30b6fa1ca2dea0b992fe9f127e125daf3abc9e0683329c7c6f6544f26a83db9dcaf56826628943e9e21b00898e933c039f7ebb86270cc07797d3d288-secret-api-key',
   residentCollectionId: '67dd9d450006fdc96d31',
   userCollectionId: '67dd9d7f002618ca37b6',
   complaintsCollectionId: '67dd9d76000e155e3b15',
   announcementCollectionId: '67dd9d62003e0b3730dd',
   documentsCollectionId: '67dd9d580012329950fc',
   profileImagesBucketId: 'profile_images',
+  platform: 'web'
 };
